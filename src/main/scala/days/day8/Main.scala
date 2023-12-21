c35e855be4060dd4e3f5c65d3a3c84b97393df4a
sko2 <sko2.dev@gmail.com>
2023-12-21 13:23:45 -0600

    Day 8 part 2

--- src/main/scala/days/day8/Main.scala
+++ src/main/scala/days/day8/Main.scala
@@ -8,14 +8,50 @@ import scala.util.matching.Regex
 
 @main
 def main(args: String*): Unit =
-  val result = partOneSolution()
+  val result = partTwoSolution()
   println(result)
 
+def partTwoSolution(): Long =
+  def isEndNodeFn(nodeName: String): Boolean = nodeName.endsWith("Z")
+
+  def calculateLowestCommonMultiple(xs: List[Long]): Long =
+    xs.reduce(lowestCommonMultiple)
+
+  def lowestCommonMultiple(a: Long, b: Long): Long =
+    (math.abs(a) * math.abs(b)) / greatestCommonDivisor(a, b)
+
+  def greatestCommonDivisor(a: Long, b: Long): Long =
+    @tailrec
+    def gcd(a: Long, b: Long): Long =
+      require(a >= b)
+      if b == 0 then
+        a
+      else
+        gcd(b, a % b)
+    if a > b then
+      gcd(a, b)
+    else
+      gcd(b, a)
+
+  val (directions, nodeMap) = parseDirectionsAndNodeMap()
+  val allSimultaneousPathBaseLengths = nodeMap.keys
+    .filter(k => k.endsWith("A"))
+    .map(countStepsToEnd(_, isEndNodeFn, directions, nodeMap))
+    .map(_.toLong)
+    .toList
+  val lcmOfSimultaneousPathBaseLengths = calculateLowestCommonMultiple(allSimultaneousPathBaseLengths)
+  lcmOfSimultaneousPathBaseLengths
+
 def partOneSolution(): Int =
+  def isEndNodeFn(nodeName: String): Boolean = nodeName == "ZZZ"
+  val (directions, nodeMap) = parseDirectionsAndNodeMap()
+  countStepsToEnd("AAA", isEndNodeFn, directions, nodeMap)
+
+def parseDirectionsAndNodeMap(): (List[Direction], Map[String, Node]) =
   val lines = InputFileReader.getLinesFromFile("./src/main/scala/days/day8/input.txt")
   val directions = parseDirections(lines.head)
   val nodeMap = lines.drop(2).map(parseNodeDefinition).toMap
-  countStepsToEnd(directions, nodeMap)
+  (directions, nodeMap)
 
 def parseDirections(line: String): List[Direction] =
   def charToDirection(char: Char): Direction =
@@ -38,7 +74,7 @@ def parseNodeDefinition(line: String): (String, Node) =
       (nodeName, Node(leftChildNode, rightChildNode))
     case _ => throw RuntimeException(s"Did not find expected nodeDefinitionRegex match in input: $line")
 
-def countStepsToEnd(directions: List[Direction], nodeMap: Map[String, Node]): Int =
+def countStepsToEnd(initialNode: String, endNodeQualifier: String => Boolean, directions: List[Direction], nodeMap: Map[String, Node]): Int =
   @tailrec
   def findStepsToDestination(startNode: String, acc: Int = 1): Int =
     def traverse(direction: Direction, node: Node): String =
@@ -50,10 +86,10 @@ def countStepsToEnd(directions: List[Direction], nodeMap: Map[String, Node]): In
       directions.foldLeft(startNode)((currentNode, direction) => traverse(direction, nodeMap(currentNode)))
 
     val destinationAfterFollowingDirections = followDirections(startNode)
-    if destinationAfterFollowingDirections == "ZZZ" then
+    if endNodeQualifier(destinationAfterFollowingDirections) then
       acc
     else
       findStepsToDestination(destinationAfterFollowingDirections, acc + 1)
 
-  val numberOfTimesNeededToFollowDirections = findStepsToDestination("AAA")
+  val numberOfTimesNeededToFollowDirections = findStepsToDestination(initialNode)
   numberOfTimesNeededToFollowDirections * directions.length
