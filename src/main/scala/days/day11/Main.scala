a8db3d29c680c72fb67f40ad1b1e4c56edc2f5ed
sko2 <sko2.dev@gmail.com>
2024-01-15 18:17:24 -0600

    Day 11 part 2

--- src/main/scala/days/day11/Main.scala
+++ src/main/scala/days/day11/Main.scala
@@ -6,13 +6,19 @@ import scala.annotation.tailrec
 
 @main
 def main(args: String*): Unit =
-  val solution = partOneSolution()
+  val solution = partTwoSolution()
   println(solution)
   
-def partOneSolution(): Int =
+def partOneSolution(): Long =
+  solution(expandingDistance = 1)
+
+def partTwoSolution(): Long =
+  solution(expandingDistance = 999999)
+
+def solution(expandingDistance: Int): Long =
   val lines = InputFileReader.getLinesFromFile("./src/main/scala/days/day11/input.txt").toList
   val expandedGalaxyMap = expandGalaxies(lines)
-  val galaxies = getGalaxyCoordinates(expandedGalaxyMap)
+  val galaxies = getGalaxyCoordinates(expandedGalaxyMap, expandingDistance = expandingDistance)
   val galaxyPairs = galaxies.combinations(2).toList
   val shortestPaths = galaxyPairs.map(pair => calculateManhattanDistance(pair(0), pair(1)))
   val solution = shortestPaths.sum
@@ -24,13 +30,13 @@ def expandGalaxies(galaxyInput: List[String]): List[String] =
   @tailrec
   def expandGalaxiesByRow(galaxyInput: List[String], acc: List[String] = List.empty): List[String] =
     def isEmptyLine(line: String) = !line.contains('#')
-    def generateEmptyLine(lineLength: Int) = "." * lineLength
+    def generateExpandingLine(lineLength: Int) = "*" * lineLength
 
     galaxyInput match
       case Nil => acc.reverse
       case x :: xs =>
         if isEmptyLine(x) then
-          val updatedAcc = generateEmptyLine(x.length) :: x :: acc
+          val updatedAcc = generateExpandingLine(x.length) :: x :: acc
           expandGalaxiesByRow(xs, updatedAcc)
         else
           val updatedAcc = x :: acc
@@ -42,17 +48,36 @@ def expandGalaxies(galaxyInput: List[String]): List[String] =
   val fullyExpandedGalaxies = expandGalaxiesByRow(expandedGalaxiesByColumn)
   fullyExpandedGalaxies
 
-def getGalaxyCoordinates(galaxyMap: List[String]): List[(Int, Int)] =
-  def galaxyRegex = "(#)".r
+def getGalaxyCoordinates(galaxyMap: List[String], expandingDistance: Int = 999999): List[(Long, Long)] =
+  def getGalaxyIndices(galaxyLine: String): List[Long] =
+    @tailrec
+    def parseGalaxyLine(galaxyInput: List[Char], columnStart: Long = 0, acc: List[Long] = List.empty): List[Long] =
+      galaxyInput match
+        case Nil => acc
+        case x :: xs =>
+          x match
+            case '.' => parseGalaxyLine(xs, columnStart + 1, acc)
+            case '*' => parseGalaxyLine(xs, columnStart + expandingDistance, acc)
+            case '#' =>
+              val updatedAcc = columnStart :: acc
+              parseGalaxyLine(xs, columnStart + 1, updatedAcc)
+            case _ => throw RuntimeException("Unexpected input.")
+
+    parseGalaxyLine(galaxyLine.toList)
   @tailrec
-  def scanGalaxiesByLine(galaxyLines: List[String], currentRow: Int = 0, acc: List[(Int, Int)] = List.empty): List[(Int, Int)] =
+  def scanGalaxiesByLine(galaxyLines: List[String], currentRow: Long = 0, acc: List[(Long, Long)] = List.empty): List[(Long, Long)] =
+    def isExpandingRow(line: String): Boolean =
+      line.head == '*'
     galaxyLines match
       case Nil => acc.reverse
       case x :: xs =>
-        val galaxyMatches = galaxyRegex.findAllMatchIn(x)
-        val updatedAcc = galaxyMatches.map(m => (m.start, currentRow)).toList ::: acc
-        scanGalaxiesByLine(xs, currentRow + 1, updatedAcc)
+        if isExpandingRow(x) then
+          scanGalaxiesByLine(xs, currentRow + expandingDistance, acc)
+        else
+          val galaxyIndices = getGalaxyIndices(x)
+          val updatedAcc = galaxyIndices.map(i => (i, currentRow)) ::: acc
+          scanGalaxiesByLine(xs, currentRow + 1, updatedAcc)
   scanGalaxiesByLine(galaxyMap)
 
-def calculateManhattanDistance(p1: (Int, Int), p2: (Int, Int)): Int =
+def calculateManhattanDistance(p1: (Long, Long), p2: (Long, Long)): Long =
   math.abs(p1._1 - p2._1) + math.abs(p1._2 - p2._2)
